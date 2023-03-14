967157950e4d9f8793e53bf279e6a857e69f2749
oc-garcia <octaviogarcia1988@gmail.com>
2023-03-14 20:57:54 -0300

    style improvements

--- src/Components/TimeTrackerCard/index.jsx
+++ src/Components/TimeTrackerCard/index.jsx
@@ -4,15 +4,15 @@ import ellipsis from "./icon-ellipsis.svg";
 export default function TimeTrackerCard({ title, actual, previous, theme }) {
   return (
     <div className="TrackerContainer">
-      <div className={`Header ${theme}`}></div>
+      <div className={`Header ${theme}`}><h1>header</h1></div>
       <div className="CardInfo">
         <div className="TrackerTitleContainer">
           <h2>{title}</h2>
           <img src={ellipsis} alt="Options" />
         </div>
         <div className="HoursContainer">
-          <p className="Actual">{actual} hrs </p>
-          <p className="Previous">Last day - {previous} hrs</p>
+          <p className="Actual">{actual}hrs </p>
+          <p className="Previous">Last day - {previous}hrs</p>
         </div>
       </div>
     </div>
